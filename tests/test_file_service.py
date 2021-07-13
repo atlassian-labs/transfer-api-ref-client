2507421e44c6351087f9b87b95e5cd38fb5b81ef
mkameniev <71391180+mkameniev@users.noreply.github.com>
2021-07-13 17:37:10 +0300

    set static chunk size
    * set static chunk size
    * Increase number of workers

--- tests/test_file_service.py
+++ tests/test_file_service.py
@@ -6,26 +6,6 @@ from src.file_service import FileService
 
 class TestClass(unittest.TestCase):
 
-    @patch("os.path.getsize")
-    def test_block_size_should_be_default(self, getsize):
-        # mock
-        getsize.return_value = 128
-        default_block_size = 4 * 1024 * 1024
-
-        block_size = FileService.get_block_size("test")
-
-        self.assertEqual(default_block_size, block_size)
-
-    @patch("os.path.getsize")
-    def test_block_size_when_chunks_exceeded_limit(self, getsize):
-        # mock
-        getsize.return_value = 4 * 1024 * 1024 * 1024 * 1024
-        default_block_size = 4 * 1024 * 1024
-
-        block_size = FileService.get_block_size("test")
-
-        self.assertNotEqual(default_block_size, block_size)
-
     def test_generate_etag(self):
         expected_etag = 'a17c9aaa61e80a1bf71d0d850af4e5baa9800bbd-4'
 
